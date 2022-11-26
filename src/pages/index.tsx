133f8319ef0afcc1a85436b9d07b9a48332a310b
Prakhar Nagpal <prakhar.nagpal03@gmail.com>
2022-11-26 11:37:09 +0000

    initial setup

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import { type NextPage } from "next";
+import Head from "next/head";
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>AboveAverage</title>
+      </Head>
+      <main></main>
+    </>
+  );
+};
+
+export default Home;
